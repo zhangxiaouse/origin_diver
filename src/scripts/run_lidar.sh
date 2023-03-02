324d0161bf1e26bfc9185eb7fd14928659957722
ztyu <peiwvy@163.com>
2023-03-02 14:38:46 +0800

    init commit

---
+++ src/scripts/run_lidar.sh
@@ -0,0 +1,2 @@
+source ../../devel/setup.bash
+gnome-terminal --tab --title "lidar driver" -e 'bash -c "roslaunch livox_ros_driver2 rviz_MID360.launch "'
---
+++ src/scripts/run_serial.sh
@@ -0,0 +1,2 @@
+source ../../devel/setup.bash
+gnome-terminal --tab --title "serial driver" -e 'bash -c "roslaunch serial_rs232 serial_pub_sub.launch  "'
