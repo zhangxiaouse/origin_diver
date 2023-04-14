08e49a6ea2450e18393fe688f863691257bfc25a
ztyu <peiwvy@163.com>
2023-04-14 15:52:18 +0800

    add receive thread

--- src/serial_rs232/src/serial_pub_sub.cpp
+++ src/serial_rs232/src/serial_pub_sub.cpp
@@ -18,12 +18,65 @@
 
 #include <algorithm>
 #include <iostream>
+#include <thread>
 #include <vector>
+
+ros::Timer publish_timer;
+
+ros::Publisher pubSpeed;
+ros::Publisher pubImu;
 
 std::string speed_topic = "/speed_feedback";
 std::string imu_topic = "/imu_virtual";
 std::string localization_topic = "/localization/estimation_odom";
 double wheel_distance = 0.64;
+
+class SerialPort {
+public:
+  SerialPort(const std::string &port, int baudrate)
+      : port_(port), baudrate_(baudrate) {
+    ser_.setPort(port_);
+    ser_.setBaudrate(baudrate_);
+    ser_.setTimeout(to_);
+  }
+  ~SerialPort() { ser_.close(); }
+
+  bool open() {
+    try {
+      ser_.open();
+    } catch (serial::IOException &e) {
+      ROS_ERROR("Unable to open %s!", port_.c_str());
+      return false;
+    }
+    if (ser_.isOpen()) {
+      ROS_INFO("%s is opened.", port_.c_str());
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool isOpen() const { return ser_.isOpen(); }
+
+  void read(std::vector<unsigned char> &buffer) {
+    auto num = ser_.available();
+    std::vector<unsigned char> recv_data(num);
+    ser_.read(recv_data.data(), num);
+    buffer.insert(buffer.end(), recv_data.begin(), recv_data.end());
+  }
+
+  void write(const std::vector<unsigned char> &buffer) {
+    ser_.write(buffer.data(), buffer.size());
+  }
+
+private:
+  serial::Serial ser_;
+  std::string port_;
+  int baudrate_;
+  serial::Timeout to_ = serial::Timeout::simpleTimeout(20);
+};
+
+std::shared_ptr<SerialPort> serial_ptr_;
 
 struct Datapub {
   u_char head;
@@ -105,110 +158,23 @@ void localizationCallback(const nav_msgs::OdometryConstPtr &msg) {
   }
 }
 
-class SerialPort {
-public:
-  SerialPort(const std::string &port, int baudrate)
-      : port_(port), baudrate_(baudrate) {
-    ser_.setPort(port_);
-    ser_.setBaudrate(baudrate_);
-    ser_.setTimeout(to_);
-  }
-  ~SerialPort() { ser_.close(); }
-
-  bool open() {
-    try {
-      ser_.open();
-    } catch (serial::IOException &e) {
-      ROS_ERROR("Unable to open %s!", port_.c_str());
-      return false;
-    }
-    if (ser_.isOpen()) {
-      ROS_INFO("%s is opened.", port_.c_str());
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  bool isOpen() const { return ser_.isOpen(); }
-
-  void read(std::vector<unsigned char> &buffer) {
-    auto num = ser_.available();
-    std::vector<unsigned char> recv_data(num);
-    ser_.read(recv_data.data(), num);
-    buffer.insert(buffer.end(), recv_data.begin(), recv_data.end());
-  }
-
-  void write(const std::vector<unsigned char> &buffer) {
-    ser_.write(buffer.data(), buffer.size());
-  }
-
-private:
-  serial::Serial ser_;
-  std::string port_;
-  int baudrate_;
-  serial::Timeout to_ = serial::Timeout::simpleTimeout(20);
-};
-
-// void speed_data_tomsg(){
-
-//     speed_data_msg.header.stamp = ros::Time::now();
-//     speed_data_msg.speed_cmps = (dataSub.speed_left + dataSub.speed_right) *
-//     0.1 * 0.5; speed_data_msg.speed_kmph = (dataSub.speed_left +
-//     dataSub.speed_right) * 0.001 * 0.5 * 3.6;
-// }
-
-int main(int argc, char *argv[]) {
-  ros::init(argc, argv, "serial_pub_sub");
-  ros::NodeHandle n("~");
-
-  std::string serial_port = "/dev/ttyUSB0";
-  int baudrate = 115200;
-  n.param("serial_port", serial_port, serial_port);
-  n.param("baudrate", baudrate, baudrate);
-  n.param("speed_topic", speed_topic, speed_topic);
-  n.param("imu_topic", imu_topic, imu_topic);
-  n.param("localization_topic", localization_topic, localization_topic);
-  n.param("wheel_distance", wheel_distance, wheel_distance);
-
-  std::cout << serial_port << std::endl;
-
-  auto pubSpeed = n.advertise<cyber_msgs::SpeedFeedbackAGV>(speed_topic, 100);
-  auto pubImu = n.advertise<sensor_msgs::Imu>(imu_topic, 100);
-  auto sublocalization =
-      n.subscribe(localization_topic, 1, localizationCallback);
-
-  SerialPort serial(serial_port, baudrate);
-  if (!serial.open()) {
-    return -1;
-  }
-
-  ros::Rate loop_rate(20);
+void reveive() {
   std::vector<unsigned char> read_buffer;
+  const int TARGET_LENGTH = 20;
+  const unsigned char TARGET_HEADER = 0x41;
   while (ros::ok()) {
-    std::cout << "ROS-SPIN-ONCE:" << std::endl;
-    ros::spinOnce();
-    const int TARGET_LENGTH = 20;
-    const unsigned char TARGET_HEADER = 0x41;
-
-    serial.read(read_buffer);
-    std::cout << read_buffer.size() << std::endl;
+    serial_ptr_->read(read_buffer);
     while (read_buffer.size() >= TARGET_LENGTH) {
-      std::cout << "   size > length" << std::endl;
       if (read_buffer.front() != TARGET_HEADER) {
         read_buffer.erase(read_buffer.begin());
         continue;
       }
-      std::cout << "   front() == TARGET_HEADER" << std::endl;
       if (read_buffer.size() >= TARGET_LENGTH) {
         std::vector<unsigned char> parse_speed_data(TARGET_LENGTH);
         std::move(read_buffer.begin(), read_buffer.begin() + TARGET_LENGTH,
                   parse_speed_data.begin());
         read_buffer.erase(read_buffer.begin(),
                           read_buffer.begin() + TARGET_LENGTH);
-
-        // 处理提取出来的数据
-        // ROS_INFO("Begin decoding...");
         if (((parse_speed_data[18] << 8) | parse_speed_data[19]) ==
             crc16(parse_speed_data.data(), 18)) {
           dataSub.localization_num =
@@ -230,30 +196,67 @@ int main(int argc, char *argv[]) {
         }
       }
     }
-
-    std::cout << " prepare publish ros data" << std::endl;
-    std::cout << "  data" << dataSub.speed_left << ", " << dataSub.speed_right
-              << std::endl;
-
-    std::vector<unsigned char> send_localization_vector;
-    send_to_senddata(dataPub, send_localization_vector);
-    serial.write(send_localization_vector);
-
-    cyber_msgs::SpeedFeedbackAGV speed_data_msg;
-    speed_data_msg.header.stamp = ros::Time::now();
-    speed_data_msg.speed_left_cmps = dataSub.speed_left * 0.1;
-    speed_data_msg.speed_right_cmps = dataSub.speed_right * 0.1;
-    pubSpeed.publish(speed_data_msg);
-
-    sensor_msgs::Imu imu_data_msg;
-    imu_data_msg.header.stamp = ros::Time::now();
-    imu_data_msg.header.frame_id = "base_link";
-    imu_data_msg.angular_velocity.z =
-        (dataSub.speed_right - dataSub.speed_left) * 0.001 / wheel_distance;
-    pubImu.publish(imu_data_msg);
-
-    loop_rate.sleep();
   }
+}
+
+void timer20hzcb(const ros::TimerEvent &) {
+  std::cout << " prepare publish ros data" << std::endl;
+  std::cout << "  data [" << dataSub.speed_left << ", " << dataSub.speed_right
+            << "] " << std::endl;
+
+  std::vector<unsigned char> send_localization_vector;
+  send_to_senddata(dataPub, send_localization_vector);
+  serial_ptr_->write(send_localization_vector);
+
+  cyber_msgs::SpeedFeedbackAGV speed_data_msg;
+  speed_data_msg.header.stamp = ros::Time::now();
+  speed_data_msg.speed_left_cmps = dataSub.speed_left * 0.1;
+  speed_data_msg.speed_right_cmps = dataSub.speed_right * 0.1;
+  pubSpeed.publish(speed_data_msg);
+
+  sensor_msgs::Imu imu_data_msg;
+  imu_data_msg.header.stamp = ros::Time::now();
+  imu_data_msg.header.frame_id = "base_link";
+  imu_data_msg.angular_velocity.z =
+      (dataSub.speed_right - dataSub.speed_left) * 0.001 / wheel_distance;
+  pubImu.publish(imu_data_msg);
+}
+// void speed_data_tomsg(){
+//     speed_data_msg.header.stamp = ros::Time::now();
+//     speed_data_msg.speed_cmps = (dataSub.speed_left +
+//     dataSub.speed_right)
+//     * 0.1 * 0.5; speed_data_msg.speed_kmph = (dataSub.speed_left +
+//     dataSub.speed_right) * 0.001 * 0.5 * 3.6;
+// }
+
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "serial_pub_sub");
+  ros::NodeHandle n("~");
+
+  std::string serial_port = "/dev/ttyUSB0";
+  int baudrate = 115200;
+  n.param("serial_port", serial_port, serial_port);
+  n.param("baudrate", baudrate, baudrate);
+  n.param("speed_topic", speed_topic, speed_topic);
+  n.param("imu_topic", imu_topic, imu_topic);
+  n.param("localization_topic", localization_topic, localization_topic);
+  n.param("wheel_distance", wheel_distance, wheel_distance);
+
+  std::cout << serial_port << std::endl;
+
+  pubSpeed = n.advertise<cyber_msgs::SpeedFeedbackAGV>(speed_topic, 100);
+  pubImu = n.advertise<sensor_msgs::Imu>(imu_topic, 100);
+  auto sublocalization =
+      n.subscribe(localization_topic, 1, localizationCallback);
+  publish_timer = n.createTimer(ros::Duration(0.05), timer20hzcb);
+  serial_ptr_ = std::make_shared<SerialPort>(serial_port, baudrate);
+  if (!serial_ptr_->open()) {
+    return -1;
+  }
+
+  std::thread receive_thread(&reveive);
+
+  ros::spin();
 
   return 0;
 }
