688fa8b754823ea53c5be8d60b3155b30d51aebe
ztyu <peiwvy@163.com>
2023-08-07 16:03:06 +0800

    add perception stop

--- driver/serial_rs232/src/serial_pub_sub.cpp
+++ driver/serial_rs232/src/serial_pub_sub.cpp
@@ -7,34 +7,38 @@
 #include "nav_msgs/Odometry.h"
 #include "ros/ros.h"
 #include "ros/time.h"
+#include "std_msgs/Int8.h"
 #include "tf/transform_datatypes.h"
 
 #include <algorithm>
 #include <iostream>
 #include <mutex>
+#include <string>
 #include <thread>
 #include <vector>
 
 ros::Publisher pubSpeed;
 
-std::string speed_topic = "/speed_feedback";
-std::string localization_topic = "/localization/estimation_odom";
-double wheel_distance = 0.64;
+std::string speed_topic                = "/speed_feedback";
+std::string perception_emergency_topic = "/perception/emergency";
+std::string localization_topic         = "/localization/estimation_odom";
+double      wheel_distance             = 0.64;
 
 class SerialPort {
  public:
-  SerialPort(const std::string &port, int baudrate)
-      : port_(port), baudrate_(baudrate) {
+  SerialPort(const std::string& port, int baudrate) : port_(port), baudrate_(baudrate) {
     ser_.setPort(port_);
     ser_.setBaudrate(baudrate_);
     ser_.setTimeout(to_);
   }
-  ~SerialPort() { ser_.close(); }
+  ~SerialPort() {
+    ser_.close();
+  }
 
   bool open() {
     try {
       ser_.open();
-    } catch (serial::IOException &e) {
+    } catch (serial::IOException& e) {
       ROS_ERROR("Unable to open %s!", port_.c_str());
       return false;
     }
@@ -46,68 +50,71 @@ class SerialPort {
     }
   }
 
-  bool isOpen() const { return ser_.isOpen(); }
+  bool isOpen() const {
+    return ser_.isOpen();
+  }
 
-  void read(std::vector<unsigned char> &buffer) {
-    auto num = ser_.available();
+  void read(std::vector<unsigned char>& buffer) {
+    auto                       num = ser_.available();
     std::vector<unsigned char> recv_data(num);
     ser_.read(recv_data.data(), num);
     buffer.insert(buffer.end(), recv_data.begin(), recv_data.end());
   }
 
-  void write(const std::vector<unsigned char> &buffer) {
+  void write(const std::vector<unsigned char>& buffer) {
     ser_.write(buffer.data(), buffer.size());
   }
 
  private:
-  serial::Serial ser_;
-  std::string port_;
-  int baudrate_;
+  serial::Serial  ser_;
+  std::string     port_;
+  int             baudrate_;
   serial::Timeout to_ = serial::Timeout::simpleTimeout(20);
 };
 
-std::shared_ptr<SerialPort> serial_ptr_;
+std::shared_ptr<SerialPort> serial_ptr;
 
 struct Datapub {
-  u_char head;
+  u_char   head;
   uint16_t localization_num;
-  int32_t x_mm;
-  int32_t y_mm;
-  int16_t yaw_mm;
-  u_char localization_truth;
-  u_char command_info;
+  int32_t  x_mm;
+  int32_t  y_mm;
+  int16_t  yaw_mm;
+  u_char   localization_truth;
+  u_char   command_info;
+
+  bool stop_flag = false;
 
   bool isNew;
 };
-Datapub dataPub;
+Datapub    dataPub;
 std::mutex dataPub_mtx;
 
 struct Datasub {
-  u_char head;
+  u_char   head;
   uint16_t localization_num;
-  int32_t speed_left;
-  int32_t speed_right;
+  int32_t  speed_left;
+  int32_t  speed_right;
   uint32_t timestamp;
 };
-Datasub dataSub;
+Datasub    dataSub;
 std::mutex dataSub_mtx;
 
-void write_le(unsigned char *buf, int offset, uint64_t value, int size) {
+void write_le(unsigned char* buf, int offset, uint64_t value, int size) {
   for (int i = 0; i < size; i++) {
     buf[offset + i] = value & 0xFF;
     value >>= 8;
   }
 }
 
-void write_be(unsigned char *buf, int offset, uint64_t value, int size) {
+void write_be(unsigned char* buf, int offset, uint64_t value, int size) {
   for (int i = size - 1; i >= 0; i--) {
     buf[offset + i] = value & 0xFF;
     value >>= 8;
   }
 }
 
-void send_to_senddata(const Datapub &dataPub,
-                      std::vector<unsigned char> &buff) {
+void send_to_senddata(const Datapub& dataPub, std::vector<unsigned char>& buff) {
   unsigned char send_localization_data[20] = {0};
 
   write_be(send_localization_data, 0, dataPub.head, 1);
@@ -117,20 +124,31 @@ void send_to_senddata(const Datapub &dataPub,
   write_be(send_localization_data, 11, dataPub.yaw_mm, 2);
   write_be(send_localization_data, 13, dataPub.localization_truth, 1);
   write_be(send_localization_data, 14, dataPub.command_info, 1);
+  write_be(send_localization_data, 15, dataPub.stop_flag, 1);
+
   uint16_t temp_16 = crc16(send_localization_data, 18);
   write_be(send_localization_data, 18, temp_16, 2);
 
-  std::vector<unsigned char> send_localization_vector(
-      send_localization_data, send_localization_data + 20);
+  std::vector<unsigned char> send_localization_vector(send_localization_data, send_localization_data + 20);
   buff = std::move(send_localization_vector);
 }
 
-void localizationCallback(const nav_msgs::OdometryConstPtr &msg) {
+void perceptionEmergencyCallback(const std_msgs::Int8ConstPtr& msg) {
+  if (msg->data != 0) {
+    dataPub.stop_flag = 1;
+  } else {
+    dataPub.stop_flag = 0;
+  }
+
+  dataPub.isNew = true;
+}
+
+void localizationCallback(const nav_msgs::OdometryConstPtr& msg) {
   auto x = msg->pose.pose.position.x;
   auto y = msg->pose.pose.position.y;
 
   tf::Quaternion quat;
-  double roll, pitch, yaw;
+  double         roll, pitch, yaw;
   tf::quaternionMsgToTF(msg->pose.pose.orientation, quat);
   tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
 
@@ -140,13 +158,13 @@ void localizationCallback(const nav_msgs::OdometryConstPtr &msg) {
     static int localization_count = 0;
     localization_count++;
 
-    dataPub.head = 0x41;
-    dataPub.localization_num = localization_count;
-    dataPub.x_mm = int(x * 1000);
-    dataPub.y_mm = int(y * 1000);
-    dataPub.yaw_mm = float(yaw / (2 * M_PI) * 360 * 10);
+    dataPub.head               = 0x41;
+    dataPub.localization_num   = localization_count;
+    dataPub.x_mm               = int(x * 1000);
+    dataPub.y_mm               = int(y * 1000);
+    dataPub.yaw_mm             = float(yaw / (2 * M_PI) * 360 * 10);
     dataPub.localization_truth = 1;
-    dataPub.command_info = 1;
+    dataPub.command_info       = 1;
 
     dataPub.isNew = true;
   }
@@ -154,10 +172,10 @@ void localizationCallback(const nav_msgs::OdometryConstPtr &msg) {
 
 void reveive() {
   std::vector<unsigned char> read_buffer;
-  const int TARGET_LENGTH = 20;
-  const unsigned char TARGET_HEADER = 0x41;
+  const int                  TARGET_LENGTH = 20;
+  const unsigned char        TARGET_HEADER = 0x41;
   while (ros::ok()) {
-    serial_ptr_->read(read_buffer);
+    serial_ptr->read(read_buffer);
     while (read_buffer.size() >= TARGET_LENGTH) {
       if (read_buffer.front() != TARGET_HEADER) {
         read_buffer.erase(read_buffer.begin());
@@ -165,25 +183,18 @@ void reveive() {
       }
       if (read_buffer.size() >= TARGET_LENGTH) {
         std::vector<unsigned char> parse_speed_data(TARGET_LENGTH);
-        std::move(read_buffer.begin(), read_buffer.begin() + TARGET_LENGTH,
-                  parse_speed_data.begin());
-        read_buffer.erase(read_buffer.begin(),
-                          read_buffer.begin() + TARGET_LENGTH);
-        if (((parse_speed_data[18] << 8) | parse_speed_data[19]) ==
-            crc16(parse_speed_data.data(), 18)) {
+        std::move(read_buffer.begin(), read_buffer.begin() + TARGET_LENGTH, parse_speed_data.begin());
+        read_buffer.erase(read_buffer.begin(), read_buffer.begin() + TARGET_LENGTH);
+        if (((parse_speed_data[18] << 8) | parse_speed_data[19]) == crc16(parse_speed_data.data(), 18)) {
           std::lock_guard<std::mutex> lock(dataSub_mtx);
 
-          dataSub.localization_num =
-              (uint16_t)((parse_speed_data[1] << 8) | parse_speed_data[2]);
-          dataSub.speed_left = (int32_t)(
-              (parse_speed_data[3] << 24) | (parse_speed_data[4] << 16) |
-              (parse_speed_data[5] << 8) | parse_speed_data[6]);
-          dataSub.speed_right = (int32_t)(
-              (parse_speed_data[7] << 24) | (parse_speed_data[8] << 16) |
-              (parse_speed_data[9] << 8) | parse_speed_data[10]);
-          dataSub.timestamp = (int32_t)(
-              (parse_speed_data[11] << 24) | (parse_speed_data[12] << 16) |
-              (parse_speed_data[13] << 8) | parse_speed_data[14]);
+          dataSub.localization_num = (uint16_t)((parse_speed_data[1] << 8) | parse_speed_data[2]);
+          dataSub.speed_left =
+            (int32_t)((parse_speed_data[3] << 24) | (parse_speed_data[4] << 16) | (parse_speed_data[5] << 8) | parse_speed_data[6]);
+          dataSub.speed_right =
+            (int32_t)((parse_speed_data[7] << 24) | (parse_speed_data[8] << 16) | (parse_speed_data[9] << 8) | parse_speed_data[10]);
+          dataSub.timestamp =
+            (int32_t)((parse_speed_data[11] << 24) | (parse_speed_data[12] << 16) | (parse_speed_data[13] << 8) | parse_speed_data[14]);
           read_buffer.clear();
         } else {
           ROS_WARN("Invalid Data, Something went wrong...");
@@ -202,52 +213,55 @@ void send() {
       last_time = ros::Time::now().toSec();
       std::vector<unsigned char> send_localization_vector;
       send_to_senddata(dataPub, send_localization_vector);
-      serial_ptr_->write(send_localization_vector);
+
+      for (int i = 0; i < send_localization_vector.size(); i++) {
+        std::cout << std::to_string(send_localization_vector[i]) << "  ";
+      }
+
+      // std::cout << " " << std::endl;
+      serial_ptr->write(send_localization_vector);
       dataPub.isNew = false;
     }
     if (ros::Time::now().toSec() - last_time >= 0.05) {
-      ROS_WARN("Localization data lost about : %lfs.",
-               ros::Time::now().toSec() - last_time);
+      ROS_WARN("Localization data lost about : %lfs.", ros::Time::now().toSec() - last_time);
     }
 
     {
-      std::lock_guard<std::mutex> lock(dataSub_mtx);
+      std::lock_guard<std::mutex>   lock(dataSub_mtx);
       cyber_msgs::SpeedFeedbackDiff speed_data_msg;
-      speed_data_msg.header.stamp = ros::Time::now();
-      speed_data_msg.speed_left_cmps = dataSub.speed_left * 0.1;
+      speed_data_msg.header.stamp     = ros::Time::now();
+      speed_data_msg.speed_left_cmps  = dataSub.speed_left * 0.1;
       speed_data_msg.speed_right_cmps = dataSub.speed_right * 0.1;
-      speed_data_msg.angular_velocity =
-          (dataSub.speed_right - dataSub.speed_left) * 0.001 / wheel_distance;
+      speed_data_msg.angular_velocity = (dataSub.speed_right - dataSub.speed_left) * 0.001 / wheel_distance;
       pubSpeed.publish(speed_data_msg);
-      std::cout << " publish ros data [" << dataSub.speed_left << ", "
-                << dataSub.speed_right << "] " << std::endl;
+      std::cout << " publish ros data [" << dataSub.speed_left << ", " << dataSub.speed_right << "] " << std::endl;
     }
 
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
   }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   ros::init(argc, argv, "serial_pub_sub");
   ros::NodeHandle n("~");
 
   std::string serial_port = "/dev/ttyUSB0";
-  int baudrate = 115200;
+  int         baudrate    = 115200;
   n.param("serial_port", serial_port, serial_port);
   n.param("baudrate", baudrate, baudrate);
   n.param("speed_topic", speed_topic, speed_topic);
   n.param("localization_topic", localization_topic, localization_topic);
   n.param("wheel_distance", wheel_distance, wheel_distance);
 
-  std::cout << serial_port << std::endl;
+  std::cout << serial_port << "1" << std::endl;
 
   pubSpeed = n.advertise<cyber_msgs::SpeedFeedbackDiff>(speed_topic, 100);
 
-  auto sublocalization =
-      n.subscribe(localization_topic, 1, localizationCallback);
+  auto sublocalization     = n.subscribe(localization_topic, 1, localizationCallback);
+  auto perceptionEmergency = n.subscribe(perception_emergency_topic, 1, perceptionEmergencyCallback);
 
-  serial_ptr_ = std::make_shared<SerialPort>(serial_port, baudrate);
-  if (!serial_ptr_->open()) {
+  serial_ptr = std::make_shared<SerialPort>(serial_port, baudrate);
+  if (!serial_ptr->open()) {
     return -1;
   }
 
